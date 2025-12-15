7338b0fdaa4080856be3366b76dc67194b08f19e
Charles Christensen <30779702+charlesrc019@users.noreply.github.com>
2025-12-14 20:47:10 -0700

    Update sleep duration and log messages in script

--- mac-cleaner.sh
+++ mac-cleaner.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-# Wait 2 mintues after boot to start.
+# Wait until after boot to start.
 echo "Starting mac-cleaner.sh on $(date) as $(whoami)..." > /Library/Logs/mac-cleaner.log
 echo " " >> /Library/Logs/mac-cleaner.log
 echo "Waiting for initialization before working..." >> /Library/Logs/mac-cleaner.log
-sleep 120
+sleep 60
 
 # Define user and paths.
 USER="christensen"
@@ -15,7 +15,7 @@ DOCS_LOC="$HOME/Documents"
 TRASH="$HOME/.Trash"
 
 # Move files from Desktop.
-echo "Removing files from Desktop." >> /Library/Logs/mac-cleaner.log
+echo "Removing files from Desktop..." >> /Library/Logs/mac-cleaner.log
 find "$DESK_LOC" -maxdepth 1 -mindepth 1 -type f ! -name ".DS_Store" ! -name ".localized" -exec mv -f {} "$DOWN_LOC" \;
 
 # Move files from Documents.
@@ -28,7 +28,7 @@ done
 eval "find \"$DOCS_LOC\" -maxdepth 1 -mindepth 1 ! -name \".DS_Store\" ! -name \".localized\" $exclude_opts_docs -exec mv -f {} \"$DOWN_LOC\" \;"
 
 # Move files from root.
-#echo "Remocing files from root..." >> /Library/Logs/mac-cleaner.log
+#echo "Removing files from root..." >> /Library/Logs/mac-cleaner.log
 #excludes_root=(".file" ".nofollow" ".resolve" ".vol" ".VolumeIcon.icns" "Applications" "bin" "cores" "dev" "etc" "home" "Library" "opt" "private" "sbin" "System" "tmp" "usr" "Users" "var" "Volumes")
 #exclude_opts_root=""
 #for exc in "${excludes_root[@]}"; do
